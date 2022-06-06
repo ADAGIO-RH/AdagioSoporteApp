c4399c0bff5c9731cdf8d420ead3ee5ee736cf7a
Aneudy Abreu <aneudy91@gmail.com>
2022-06-06 17:05:11 -0500

    change api base url in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API
+    baseURL: 'https://soporte-dev-api.adagiorh.com/api/v1'
   },
   server: {
     port: process.env.PORT,
